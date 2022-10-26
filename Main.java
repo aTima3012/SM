645694560a3fa18045dd4acf03528c8ff5f580ac
aTima3012 <3012@inbox.ru>
2022-10-26 14:25:13 +0400

    22

--- Main.java
+++
@@ -1,8 +0,0 @@
-
-public class Main {
-    public static void main(String[] args) {
-
-        SalesManager salesManager = new SalesManager(new int[]{5, 6, 8, 10, 11});
-        System.out.println(salesManager.max());
-    }
-}
